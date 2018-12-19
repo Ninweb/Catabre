54aa538b1aeebb3077fe1c3023cede3668bbdd03
crarrivillaga <carlosra210@gmail.com>
2018-12-19 11:00:35 -0400

    integracion de jquery y js de interaccion de tabla

--- vista/script.php
+++ vista/script.php
@@ -2,3 +2,9 @@
     <script src="js/bootstrap.min.js"></script>
     <script src="js/sb-admin.js"></script>
     <script src="js/validar.js"></script>
+    <script src="js/tabla-constructor.js"></script>
+
+    <script
+	  src="https://code.jquery.com/jquery-3.3.1.min.js"
+	  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+	  crossorigin="anonymous"></script>
