fd6a033375306a50a673883affbdd4b1e3ddd1bc
crarrivillaga <carlosra210@gmail.com>
2018-12-19 14:35:01 -0400

    tabla productos db

--- controlador/pedido.php
+++ controlador/pedido.php
@@ -1,5 +1,7 @@
+/*
 <?php
-
+echo ('hola');
+/*
 
     function imprimirJSON($json){
         $prueba = var_dump($json);
@@ -8,25 +10,25 @@
     //$numeroPedidos = $_POST['hidden-number-pedido'];
     $numeroPedidos = 4;
     for($i = 1; $i <= $numeroPedidos ; $i++ ){
-        /*json_encode(array(
+        // json_encode(array(
 
-            "idContenedor"=>$_POST['idContenedor'],
-            "idCliente"=>$_POST['idContenedor'],
-            "numeroReferecia"=>$_POST['num_referencia'.$i],
-            "naviera"=>'naviera',
-            "destino"=>$_POST['destino'],
-            "fechaSalida"=>$_POST['salida'],
-            "fechaLlegada"=>$_POST['llegada']
-            "idContenedor"=>$_POST['idContenedor'],
-            "presentacion"=>$_POST['presentacion'.$i],
-            "producto"=>$_POST['producto'.$ii],
-            "especie"=>$_POST['especie'.$i],
-            "color"=>$_POST['color'.$i],
-            "peso"=>$_POST['peso'.$i],
-            "size"=>$_POST['size'.$i],
-            "master"=>$_POST['master'.$i],
-            "total"=>$_POST['total'.$i]
-        ));*/
+        //     "idContenedor"=>$_POST['idContenedor'],
+        //     "idCliente"=>$_POST['idContenedor'],
+        //     "numeroReferecia"=>$_POST['num_referencia'.$i],
+        //     "naviera"=>'naviera',
+        //     "destino"=>$_POST['destino'],
+        //     "fechaSalida"=>$_POST['salida'],
+        //     "fechaLlegada"=>$_POST['llegada']
+        //     "idContenedor"=>$_POST['idContenedor'],
+        //     "presentacion"=>$_POST['presentacion'.$i],
+        //     "producto"=>$_POST['producto'.$ii],
+        //     "especie"=>$_POST['especie'.$i],
+        //     "color"=>$_POST['color'.$i],
+        //     "peso"=>$_POST['peso'.$i],
+        //     "size"=>$_POST['size'.$i],
+        //     "master"=>$_POST['master'.$i],
+        //     "total"=>$_POST['total'.$i]
+        // ));
         $json = json_encode(array(
             "idContenedor"=>'contendedor'.$i,
             "idCliente"=>'cliente'.$i,
@@ -41,5 +43,5 @@
         ));
         imprimirJSON($json);
     }
-    
+    */
 ?>
