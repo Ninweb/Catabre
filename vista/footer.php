37aefcb895a0b60c4c297ff696f872d576e3d166
liseth1994 <lsanchez@ninweb.net>
2018-12-20 16:48:33 -0400

    footer

--- vista/footer.php
+++ vista/footer.php
@@ -5,7 +5,9 @@
         <footer class="sticky-footer">
           <div class="container my-auto">
             <div class="copyright text-center my-auto">
-              <span>Copyright © Your Website 2018</span>
+              <span>Copyright © CATABRE 2018 - <?php $ano=strftime("%Y"); 
+                  echo $ano;
+              ?></span>
             </div>
           </div>
         </footer>
