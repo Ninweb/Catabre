e4cb68bc5cd85eec278e32831a740c704306a1a0
crarrivillaga <carlosra210@gmail.com>
2018-12-21 10:43:46 -0400

    mejora de recarga de pagina con boton OK del mensaje

--- vista/nuevo-pedido.php
+++ vista/nuevo-pedido.php
@@ -54,12 +54,10 @@
 											include ("../modelo/conexion.php");
 											$consulta_persona = mysqli_query($db, "SELECT * FROM persona,usuario WHERE persona.id_usuario=usuario.id_usuario AND usuario.tipo='user'");
 											echo "<select name=\"id_persona\" class='col-md-10 form-control' id='users'  required>";
-											echo "<option></option>";
+											echo "<option>Seleccione usuario</option>";
 											while ($row=mysqli_fetch_array($consulta_persona)) 
 												{
-													
 													echo "<option value=\"".$row['id_persona']."\">".$row['nombre'].' '.$row['apellido'];
-
 												}
 
 											echo "</select>"; 
@@ -80,12 +78,10 @@
 											include ("../modelo/conexion.php");
 											$consulta_contenedor=mysqli_query($db, "SELECT * FROM contenedor WHERE contenedor.status='disponible'");
 											echo "<select name=\"id_contenedor\" class='col-md-10 form-control' id='contenedor'  required>";
-											echo "<option></option>";
+											echo "<option>Seleccione contenedor</option>";
 											while ($row=mysqli_fetch_array($consulta_contenedor)) 
 												{
-													
 													echo "<option id='id_contenedor' value=\"".$row['id_contenedor']."\">".$row['nombre'];
-
 												}
 											echo "</select>"; 
 										?>
