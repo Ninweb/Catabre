dd90fe8f42b94b747aa43ec9c7107cae0fbe0936
gabo431 <gabriel.enr1998@gmail.com>
2018-12-26 16:46:23 -0400

    salksla

--- controlador/usuarioPersona.php
+++ controlador/usuarioPersona.php
@@ -3,41 +3,108 @@
     include('../modelo/persona.php');
     include("../modelo/conexion.php");
 
-    function modeloUsuario($json,$db){        
-        $idUsuario = crearUsuario($json,$db);
+    function modeloUsuario($json,$db,$funcionCRUD){        
+        if($funcionCRUD == 'crear'){
+            $idUsuario = crearUsuario($json,$db);
+        }else if ($funcionCRUD == "editar"){
+            $idUsuario = editarUsuario($json,$db);
+        }else if($funcionCRUD == 'eliminar'){
+            $idUsuario = eliminarUsuario($json,$db);
+        }
         return $idUsuario;
     }
     
-    function modeloPersona($json,$db){        
-        crearPersona($json,$db);
-        
+    function modeloPersona($json,$db,$funcionCRUD){        
+        if($funcionCRUD == 'crear'){
+            crearPersona($json,$db);
+        }else if ($funcionCRUD == "editar"){
+            editarPersona($json,$db);
+        }else if($funcionCRUD == 'eliminar'){
+            eliminarPersona($json,$db);
+        }
     }
 
-    $email=$_POST['email'];
-    $password=$_POST['password'];
+    $funcionCRUD = $_POST['funcion'];
 
-    $jsonUsuario =json_encode(array(
-        "email"=>$email,
-        "password"=>$password
-    ));
+
+    if($funcionCRUD == 'crear'){
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $jsonUsuario =json_encode(array(
+            "email"=>$email,
+            "password"=>$password
+        ));
+    }else if ($funcionCRUD == 'editar'){
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+        $id_usuario = $_POST['id_usuario'];
+        $jsonUsuario =json_encode(array(
+            "email"=>$email,
+            "password"=>$password
+            "id_contenedor"=>$id_usuario
+        ));
+    }else if($funcionCRUD == 'eliminar'){
+        $id_usuario = $_POST['id_contenedor'];
+        $jsonContenedor =json_encode(array(
+            "id_contenedor"=>$id_usuario
+        ));
+    }
+
+    $idUsuario = modeloUsuario($jsonUsuario,$db,$funcionCRUD);
+
+
+    if($funcionCRUD == 'crear'){
+        $nombre=$_POST['nombre'];
+	    $apellido=$_POST['apellido'];
+        $empresa=$_POST['empresa'];
+        $jsonPersona =json_encode(array(
+            "idUsuario"=>$idUsuario,
+            "nombre"=>$nombre,
+            "apellido"=>$apellido,
+            "empresa"=>$empresa
+        ));
+    }else if ($funcionCRUD == 'editar'){
+        //buscar id de persona asociada a idUsuario
+        //$id_persona = no se 
+        $nombre=$_POST['nombre'];
+	    $apellido=$_POST['apellido'];
+        $empresa=$_POST['empresa'];
+        $jsonUsuario =json_encode(array(
+            "nombre"=>$nombre,
+            "apellido"=>$apellido,
+            "empresa"=>$empresa
+            "id_persona"=>$id_persona
+        ));
+    }else if($funcionCRUD == 'eliminar'){
+        //buscar id de persona asociada a idUsuario
+        //$id_persona = no se 
+        $jsonContenedor =json_encode(array(
+            "id_persona"=>$id_persona
+        ));
+    }
+
+
+    modeloPersona($jsonPersona,$db,$funcionCRUD);
+
     
-    $idUsuario = modeloUsuario($jsonUsuario,$db);
-
-    $nombre=$_POST['nombre'];
-	$apellido=$_POST['apellido'];
-    $empresa=$_POST['empresa'];
-    
-    $jsonPersona =json_encode(array(
-        "idUsuario"=>$idUsuario,
-        "nombre"=>$nombre,
-        "apellido"=>$apellido,
-        "empresa"=>$empresa,
-    ));
-
-    modeloPersona($jsonPersona,$db);
-
-    
-    echo "<script>swal('Guardado!', 'Nuevo Usuario', 'success');</script>";
-    echo "<script>document.getElementById('formularioUsuario').reset();</script>";
+    if($funcionCRUD=='crear'){
+        echo "<script>swal('Guardado!', 'Nuevo Usuario', 'success').then(
+            function(){ 
+                location.reload();
+            }
+        );</script>";
+    }else if ($funcionCRUD == 'editar'){
+        echo "<script>swal('Guardado!', 'Fue editado el usuario', 'success').then(
+            function(){ 
+                location.reload();
+            }
+        );</script>";
+    }else if ($funcionCRUD == 'eliminar'){
+        echo "<script>swal('Eliminado!', 'Fue eliminado el usuario', 'success').then(
+            function(){ 
+                location.reload();
+            }
+        );</script>";
+    }
 
 ?>
--- modelo/contenedor.php
+++ modelo/contenedor.php
@@ -1,7 +1,6 @@
 <?php
     function crearContenedor($json,$db){
         $contenedor = json_decode($json);
-
         $registrar=mysqli_query($db,"INSERT INTO contenedor(ref,nombre,status) VALUES('$contenedor->numeroReferencia','$contenedor->nombre','disponible')");
     }
 
